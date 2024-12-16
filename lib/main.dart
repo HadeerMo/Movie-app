7ff0aa092053324884b4ff30232fbcd5613586a8
HadeerMo <hadirhadir665@gmail.com>
2024-12-16 13:33:18 +0200

    create splash view body

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:movie_app/features/splash/presentation/views/splash_view.dart';
+void main() {
+  runApp(const MovieApp());
+}
+
+class MovieApp extends StatelessWidget {
+  const MovieApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      home: SplashView(),
+    
+    );
+  }
+}
+
