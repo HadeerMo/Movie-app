a9b3fb97cf607103cf806e80f7750b59afd14968
HadeerMo <hadirhadir665@gmail.com>
2024-12-25 15:01:12 +0200

    add title and subtitle to details

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -20,6 +20,11 @@ static const textStyle16 = TextStyle(
     fontSize: 20,
     fontWeight: FontWeight.normal,
   );
+    static const textStyle26 = TextStyle(
+    fontSize: 26,
+    fontWeight: FontWeight.normal,
+    fontFamily: kGtSectraFine,
+  );
     static const textStyle30 = TextStyle(
     fontSize: 30,
     fontWeight: FontWeight.normal,
--- lib/features/home/presentation/views/widgets/movie_details_body.dart
+++ lib/features/home/presentation/views/widgets/movie_details_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/core/utils/styles.dart';
 import 'package:movie_app/features/home/presentation/views/widgets/custom_movie_details_appbar.dart';
 import 'package:movie_app/features/home/presentation/views/widgets/custom_movie_image.dart';
 
@@ -7,15 +8,34 @@ class MovieDetailsBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 18),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 18),
       child: Column(
         children: [
-          CustomAppbarForDetails(),
-          CustomMovieImage(),
+          const CustomAppbarForDetails(),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * .35,
+            child: const CustomMovieImage(),
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          const Text(
+            'The Jungle Book',
+            style: Styles.textStyle26,
+          ),
+          const SizedBox(
+            height: 3,
+          ),
+           Opacity(
+            opacity: .7,
+            child: Text(
+              'Rudyard Kipling',
+              style: Styles.textStyle16.copyWith(fontStyle: FontStyle.italic,fontWeight: FontWeight.bold),
+            ),
+          )
         ],
       ),
     );
   }
 }
-
