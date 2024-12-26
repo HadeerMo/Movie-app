feaaaa5088a3cd14089289fa579f39d4cc5e4255
HadeerMo <hadirhadir665@gmail.com>
2024-12-26 13:12:50 +0200

    add custom scroll view

--- lib/features/home/presentation/views/widgets/movie_details_body.dart
+++ lib/features/home/presentation/views/widgets/movie_details_body.dart
@@ -11,64 +11,74 @@ class MovieDetailsBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 18),
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            const CustomAppbarForDetails(),
-            SizedBox(
-              height: MediaQuery.of(context).size.height * .35,
-              child: const CustomMovieImage(),
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 18),
+            child: Column(
+              children: [
+                const CustomAppbarForDetails(),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * .35,
+                  child: const CustomMovieImage(),
+                ),
+                const SizedBox(
+                  height: 25,
+                ),
+                const Text(
+                  'The Jungle Book',
+                  style: Styles.textStyle26,
+                ),
+                const SizedBox(
+                  height: 3,
+                ),
+                Opacity(
+                  opacity: .7,
+                  child: Text(
+                    'Rudyard Kipling',
+                    style: Styles.textStyle16.copyWith(
+                        fontStyle: FontStyle.italic,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                const SizedBox(
+                  height: 12,
+                ),
+                const MovieRate(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ),
+                const SizedBox(
+                  height: 30,
+                ),
+                const BookAction(),
+                const Expanded( //to let MoreMovieListView always in the bottom..
+                  child: SizedBox(
+                    height: 30,
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'You can also like',
+                    style: Styles.textStyle14
+                        .copyWith(fontWeight: FontWeight.w600),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                const MoreMovieListView(), //must be in the bottom of screan
+                const SizedBox(
+                  height: 20,
+                ),
+              ],
             ),
-            const SizedBox(
-              height: 25,
-            ),
-            const Text(
-              'The Jungle Book',
-              style: Styles.textStyle26,
-            ),
-            const SizedBox(
-              height: 3,
-            ),
-            Opacity(
-              opacity: .7,
-              child: Text(
-                'Rudyard Kipling',
-                style: Styles.textStyle16.copyWith(
-                    fontStyle: FontStyle.italic, fontWeight: FontWeight.bold),
-              ),
-            ),
-            const SizedBox(
-              height: 12,
-            ),
-            const MovieRate(
-              mainAxisAlignment: MainAxisAlignment.center,
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            const BookAction(),
-            const SizedBox(
-              height: 30,
-            ),
-            Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                'You can also like',
-                style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w600),
-              ),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
-            const MoreMovieListView(),
-            const SizedBox(
-              height: 40,
-            ),
-          ],
-        ),
-      ),
+          ),
+        )
+      ],
     );
+  
   }
 }
