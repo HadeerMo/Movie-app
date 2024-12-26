8e3454900362e06d534f4d3b807a1a7e43ddfea9
HadeerMo <hadirhadir665@gmail.com>
2024-12-26 13:03:08 +0200

    add see more list to details

---
+++ lib/features/home/presentation/views/widgets/more_movie_list_view.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/features/home/presentation/views/widgets/custom_movie_image.dart';
+
+class MoreMovieListView extends StatelessWidget {
+  const MoreMovieListView({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .14,
+      child: ListView.builder(
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (context, index) {
+            return const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 4),
+              child: CustomMovieImage(),
+            );
+          }),
+    );
+  }
+}
--- lib/features/home/presentation/views/widgets/movie_details_body.dart
+++ lib/features/home/presentation/views/widgets/movie_details_body.dart
@@ -3,6 +3,7 @@ import 'package:movie_app/core/utils/styles.dart';
 import 'package:movie_app/features/home/presentation/views/widgets/book_action.dart';
 import 'package:movie_app/features/home/presentation/views/widgets/custom_movie_details_appbar.dart';
 import 'package:movie_app/features/home/presentation/views/widgets/custom_movie_image.dart';
+import 'package:movie_app/features/home/presentation/views/widgets/more_movie_list_view.dart';
 import 'package:movie_app/features/home/presentation/views/widgets/movie_rating.dart';
 
 class MovieDetailsBody extends StatelessWidget {
@@ -12,39 +13,61 @@ class MovieDetailsBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 18),
-      child: Column(
-        children: [
-          const CustomAppbarForDetails(),
-          SizedBox(
-            height: MediaQuery.of(context).size.height * .35,
-            child: const CustomMovieImage(),
-          ),
-          const SizedBox(
-            height: 25,
-          ),
-          const Text(
-            'The Jungle Book',
-            style: Styles.textStyle26,
-          ),
-          const SizedBox(
-            height: 3,
-          ),
-           Opacity(
-            opacity: .7,
-            child: Text(
-              'Rudyard Kipling',
-              style: Styles.textStyle16.copyWith(fontStyle: FontStyle.italic,fontWeight: FontWeight.bold),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            const CustomAppbarForDetails(),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * .35,
+              child: const CustomMovieImage(),
             ),
-          ),
-          const SizedBox(
-            height: 12,
-          ),
-          const MovieRate(mainAxisAlignment: MainAxisAlignment.center,),
-          const SizedBox(
-            height: 30,
-          ),
-          const BookAction(),
-        ],
+            const SizedBox(
+              height: 25,
+            ),
+            const Text(
+              'The Jungle Book',
+              style: Styles.textStyle26,
+            ),
+            const SizedBox(
+              height: 3,
+            ),
+            Opacity(
+              opacity: .7,
+              child: Text(
+                'Rudyard Kipling',
+                style: Styles.textStyle16.copyWith(
+                    fontStyle: FontStyle.italic, fontWeight: FontWeight.bold),
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            const MovieRate(
+              mainAxisAlignment: MainAxisAlignment.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            const BookAction(),
+            const SizedBox(
+              height: 30,
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                'You can also like',
+                style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w600),
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            const MoreMovieListView(),
+            const SizedBox(
+              height: 40,
+            ),
+          ],
+        ),
       ),
     );
   }
