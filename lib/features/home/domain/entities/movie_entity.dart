5ed2fc31ab9715c60bd3a02e7b0d90d2a412932a
HadeerMo <hadirhadir665@gmail.com>
2024-12-29 11:58:46 +0200

    create movie entity

---
+++ lib/features/home/domain/entities/movie_entity.dart
@@ -0,0 +1,15 @@
+class MovieEntity {
+  final String image;
+  final String title;
+  final String autherName;
+  final num price;
+  final num rating;
+
+  MovieEntity(
+    this.image,
+    this.title,
+    this.autherName,
+    this.price,
+    this.rating,
+  );
+}
