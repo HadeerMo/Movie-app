eb5db709e932a7a996d0aa4b8e583d8bd877f97e
HadeerMo <hadirhadir665@gmail.com>
2024-12-29 14:03:43 +0200

    add either class

--- lib/features/home/domain/repos/home_repo.dart
+++ lib/features/home/domain/repos/home_repo.dart
@@ -1,9 +1,11 @@
+import 'package:dartz/dartz.dart';
+import 'package:movie_app/core/error/failure.dart';
 import 'package:movie_app/features/home/domain/entities/movie_entity.dart';
 
 abstract class HomeRepo {
 
-  Future<List<MovieEntity>> fetchFeaturedMovies();  //unimplemented method
+  Future<Either<Failure,List<MovieEntity>>> fetchFeaturedMovies();  //unimplemented method
 
-  Future<List<MovieEntity>> fetchNewsMovies();  //unimplemented method
-  
+  Future<Either<Failure,List<MovieEntity>>> fetchNewsMovies();  //unimplemented method
+
 }
