40bf42aa6fbf71523a59c9b8c404df4f44ac3f76
HadeerMo <hadirhadir665@gmail.com>
2024-12-30 12:50:34 +0200

    use generic use case

---
+++ lib/core/use_cases/use_case.dart
@@ -0,0 +1,13 @@
+
+import 'package:dartz/dartz.dart';
+import 'package:movie_app/core/error/failure.dart';
+
+abstract class UseCase<Type, Param> {
+  //Generic use case
+  Future<Either<Failure, Type>> call([Param param]); //optional
+}
+
+
+class NoParam {
+  //use it or use void
+}
--- lib/features/home/domain/use_cases/fetch_featured_movies_use_case.dart
+++ lib/features/home/domain/use_cases/fetch_featured_movies_use_case.dart
@@ -1,14 +1,19 @@
 import 'package:dartz/dartz.dart';
 import 'package:movie_app/core/error/failure.dart';
+import 'package:movie_app/core/use_cases/use_case.dart';
 import 'package:movie_app/features/home/domain/entities/movie_entity.dart';
 import 'package:movie_app/features/home/domain/repos/home_repo.dart';
 
-class FetchFeaturedMoviesUseCase {
+class FetchFeaturedMoviesUseCase extends UseCase<List<MovieEntity>, NoParam> {
   final HomeRepo homeRepo;
 
   FetchFeaturedMoviesUseCase(this.homeRepo);
-  Future<Either<Failure, List<MovieEntity>>> fetchFeaturedMovies() {
-    return homeRepo.fetchFeaturedMovies();
+
+  @override
+  Future<Either<Failure, List<MovieEntity>>> call ([NoParam? param]) async { //optinal paramter
+  
+    return await homeRepo.fetchFeaturedMovies();
   }
-  //if you does not need use case so don't use it
+
+  //if you do not have to use 'use case' so don't use it!
 }
