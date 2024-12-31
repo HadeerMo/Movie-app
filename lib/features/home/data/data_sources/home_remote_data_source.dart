984aad7a737b34044d38ad0ffe1d0a3de73bce04
HadeerMo <hadirhadir665@gmail.com>
2024-12-31 15:12:20 +0200

    implement remote fetch featured Movies

--- lib/features/home/data/data_sources/home_remote_data_source.dart
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -1,19 +1,29 @@
+import 'package:movie_app/core/utils/api_service.dart';
+import 'package:movie_app/features/home/data/models/movie_model/movie_model.dart';
 import 'package:movie_app/features/home/domain/entities/movie_entity.dart';
 
 abstract class HomeRemoteDataSource {
-    Future<List<MovieEntity>>
-      fetchFeaturedMovies(); //unimplemented method
+  Future<List<MovieEntity>> fetchFeaturedMovies(); //unimplemented method
 
-  Future<List<MovieEntity>>
-      fetchNewsMovies(); //unimplemented method
+  Future<List<MovieEntity>> fetchNewsMovies(); //unimplemented method
 }
 
-class HomeRemoteDataSourceImpl extends HomeRemoteDataSource{
+class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
+  final ApiService apiService;
+
+  HomeRemoteDataSourceImpl(this.apiService);
+
   @override
-  Future<List<MovieEntity>> fetchFeaturedMovies() {
-    // TODO: implement fetchFeaturedMovies
-    throw UnimplementedError();
+  Future<List<MovieEntity>> fetchFeaturedMovies() async {
+    var data = await apiService.get(
+        endPoint:
+            'advancedsearch?start_year=1970&end_year=2020&min_imdb=1&max_imdb=10&language=english&type=movie&sort=latest&page=1');
+
+    List<MovieEntity> movies = getMoviesList(data);
+    return movies;
   }
+
+
 
   @override
   Future<List<MovieEntity>> fetchNewsMovies() {
@@ -21,4 +31,14 @@ class HomeRemoteDataSourceImpl extends HomeRemoteDataSource{
     throw UnimplementedError();
   }
 
+
+
+    List<MovieEntity> getMoviesList(Map<String, dynamic> data) {
+      List<MovieEntity> movies = [];
+    for (var movie in data["results"]) {
+      movies.add(
+          MovieModel.fromJson(movie)); // movieModel extends from movieEntity
+    }
+    return movies;
+  }
 }
