10b01ef1dd777f38f9d7a7ea4eb4644cc8cd2889
HadeerMo <hadirhadir665@gmail.com>
2024-12-31 15:20:38 +0200

    implement remote fetch newest movies

--- lib/features/home/data/data_sources/home_remote_data_source.dart
+++ lib/features/home/data/data_sources/home_remote_data_source.dart
@@ -17,24 +17,23 @@ class HomeRemoteDataSourceImpl extends HomeRemoteDataSource {
   Future<List<MovieEntity>> fetchFeaturedMovies() async {
     var data = await apiService.get(
         endPoint:
-            'advancedsearch?start_year=1970&end_year=2020&min_imdb=1&max_imdb=10&language=english&type=movie&sort=latest&page=1');
+            'advancedsearch?start_year=1970&end_year=2020&min_imdb=1&max_imdb=10&language=english&sort=latest&page=1');
 
     List<MovieEntity> movies = getMoviesList(data);
     return movies;
   }
 
-
-
   @override
-  Future<List<MovieEntity>> fetchNewsMovies() {
-    // TODO: implement fetchNewsMovies
-    throw UnimplementedError();
+  Future<List<MovieEntity>> fetchNewsMovies() async {
+    var data = await apiService.get(
+        endPoint:
+            'advancedsearch?start_year=2020&end_year=2020&min_imdb=1&max_imdb=10&language=english&sort=latest&page=1');
+    List<MovieEntity> movies = getMoviesList(data);
+    return movies;
   }
 
-
-
-    List<MovieEntity> getMoviesList(Map<String, dynamic> data) {
-      List<MovieEntity> movies = [];
+  List<MovieEntity> getMoviesList(Map<String, dynamic> data) {
+    List<MovieEntity> movies = [];
     for (var movie in data["results"]) {
       movies.add(
           MovieModel.fromJson(movie)); // movieModel extends from movieEntity
